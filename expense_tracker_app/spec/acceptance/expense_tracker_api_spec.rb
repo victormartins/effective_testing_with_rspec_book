17638753ec8871dc86d007b2039f5856c359d217
Victor Martins <victor.martins@sage.com>
2017-09-25 13:46:12 +0100

    WIP

--- expense_tracker_app/spec/acceptance/expense_tracker_api_spec.rb
+++ expense_tracker_app/spec/acceptance/expense_tracker_api_spec.rb
@@ -12,18 +12,41 @@ module ExpenseTracker
       ExpenseTracker::API.new
     end
 
-    it 'records submitted expenses' do
-      coffee = {
-        'payee'  => 'Starbucks',
-        'amount' => BigDecimal.new('5.75'),
-        'date'   => '2017-06-10'
-      }
-
-      post '/expenses', JSON.generate(coffee)
-
+    def post_expense(expense)
+      post '/expenses', JSON.generate(expense)
       expect(last_response.status).to eq(200)
+
       parsed = JSON.parse(last_response.body)
       expect(parsed).to include('expense_id' => a_kind_of(String))
+
+      expense.merge(parsed['expense_id'])
+    end
+
+    it 'records submitted expenses' do
+      # coffee = post_expense(
+      #   'payee'  => 'Starbucks',
+      #   'amount' => BigDecimal.new('5.75'),
+      #   'date'   => '2017-06-10'
+      # )
+      #
+      # zoo = post_expense(
+      #   'payee'  => 'Zoo',
+      #   'amount' => BigDecimal.new('15.25'),
+      #   'date'   => '2017-06-10'
+      # )
+      #
+      # groceries = post_expense(
+      #   'payee'  => 'Whole Foods',
+      #   'amount' => BigDecimal.new('95.20'),
+      #   'date'   => '2017-06-11'
+      # )
+
+      puts ''
+      puts '-' * 50
+      puts "I'M ON PAGE 55".center(50)
+      puts '-' * 50
+      puts ''
+
     end
   end
 end
